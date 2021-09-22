f8255b0f32eb3b1be3d2aa53d065b4e1ce556d71
tiger3120 <zhek4770@gmail.com>
2021-09-22 23:39:08 +0300

    Finished

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,6 +1,6 @@
+// Useful link https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
 const fs = require('fs')
 const csv = require('csv-parser')
-const people = []
 
 fs.createReadStream('users1.csv')
     .pipe(csv({
@@ -10,21 +10,38 @@ fs.createReadStream('users1.csv')
 
         let user = {
             name: `${row['first_name']} ${row['last_name']}`,
-            // phone: ,
-            // person: {
-            //     firstName: {
-            //         type: 
-            //     },
-            //     lastName: {
-            //         type: 
-            //     },
-            // },
-            // amount: ,
-            // date: ,
-            // costCenterNum: 
+            phone: row.phone.match(/\d+/g).join(''),
+            person: {
+                firstName: {
+                    type: row['first_name']
+                },
+                lastName: {
+                    type: row['last_name']
+                },
+            },
+            amount: Math.round(parseFloat(row.amount) * 100) / 100,
+            date: formatDate(row.date.split("/").reverse().join("-")),
+            costCenterNum: row.cc.match(/\d+/)[0]
         }
 
-        
-        console.log(user)
+        // Stringify data
+        const userJSON = JSON.stringify(user);
+
+        // Write data into file
+        fs.writeFileSync('users.json', userJSON)
 
     })
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
