d2c85cea3a0acde5dcda0828b201a2f834592825
tiger3120 <zhek4770@gmail.com>
2021-09-22 21:41:37 +0300

    Commented out

--- novyzabor.js
+++ novyzabor.js
@@ -10,12 +10,13 @@ config_b.items = item2
 config_b.items.named_item = 123
 config_c.root.a.b.c = 13
 
+/*/ /*
 Expected output for sample input:
 {
    "config":3,
    "config_b":{
       "items":{
-
+         
          "0":"item1",
          "1":"item2",
 
@@ -33,23 +34,60 @@ Expected output for sample input:
    }
 } */
 
-/*
+let counterObject = {}
+let objectForJSON = {}
 
+const set = (obj, path, val) => {
 
-*/
+   // Split the path
+   let keys = path.split('.');
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+   // Last key
+   const lastKey = keys.pop();
 
-var readline = require('readline');
-var rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout,
-   terminal: false
-});
+   // Key before last
+   let lastObj = keys.reduce((obj, key) =>
+      obj[key] = obj[key] || {},
+      obj);
 
-let object = {}
+   // Path already exists
+   if (counterObject.hasOwnProperty(path)) {
 
-rl.on('line', function count(line) {
+      counterObject[path]++
 
-})
+      // Fix the first replacement
+      if (counterObject[path] === 1) {
+         lastObj[lastKey] = {
+            "0": lastObj[lastKey],
+            "1": val
+
+         }
+         // Fix all the others replacement
+      } else {
+         lastObj[lastKey][counterObject[path]] = val
+      }
+
+      // Add first value
+   } else {
+      counterObject[path] = 0
+      lastObj[lastKey] = val;
+   }
+};
+
+function addToObject(line) {
+
+   const splitedLine = line.split(" ")
+   const key = splitedLine[0]
+   const value = splitedLine[2]
+
+   set(objectForJSON, key, value)
+
+}
+
+addToObject('config = 3')
+addToObject('config_b.items = item1')
+addToObject('config_b.items = item2')
+addToObject('config_b.items.named_item = 123')
+addToObject('config_c.root.a.b.c = 13')
+
+console.log(JSON.stringify(objectForJSON))
